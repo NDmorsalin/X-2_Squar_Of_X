c9e823a0b7ee95e74dea789dfa58e1f51dbed7f1
NDmorsalin <morsalin056@gmail.com>
2020-02-08 08:46:10 +0600

    X^2_Squar_Of_X

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 
-console.log("Hello World");
+// find the value of x^2 
+   var ndMath = {
+       square : function ( x ){
+           var result = 1;
+           for(var i = 1;i <= 2; i++){
+               result *= x;
+           }
+           return result;
+       }
+   };
+   console.log(ndMath.square(7));
